af722ed9fd5c32f57dc0667d85c1535c338a638d
meecher <marcullmann@web.de>
2021-05-25 15:09:39 +0200

    test

---
+++ main.py
@@ -0,0 +1,10 @@
+from cursebox import *
+with Cursebox() as cb:
+    width, height = cb.width, cb.height
+    greeting = "Hello, World!"
+    # Center text on the screen
+    cb.put(x=(width - len(greeting)) / 2,
+    y=height / 2, text=greeting,
+    fg=colors.black, bg=colors.white)
+    # Wait for any keypress
+    cb.poll_event()
