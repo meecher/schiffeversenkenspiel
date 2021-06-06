e18718bc343940d175a87b6a060368d2e028f3a7
meecher <marcullmann@web.de>
2021-06-06 19:47:28 +0200

    Co-authored-by: daya166 <daya166@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import random, time
 from cursebox import *
-from cursebox.constants import EVENT_BACKSPACE, EVENT_ENTER
+from cursebox.constants import EVENT_BACKSPACE, EVENT_ENTER, EVENT_ESC, EVENT_LEFT, EVENT_UP
 
 
 __author__ = "1359831, Ruschmaritsch, 1357985, Ullmann"
@@ -22,24 +22,36 @@ def beginn_screen(width, height):
         cb.put(x=(width - len(comp)) / 2,
             y=height / 2, text=comp,
             fg=colors.black, bg=colors.white)
-        cb.put(x=(width - len(mp)) / 2,
-            y=height / 2, text=mp,
-            fg=colors.black, bg=colors.white)
+        cb.refresh()
+        cb.poll_event()
+        #cb.put(x=(width - len(mp)) / 2,
+        #    y=height / 2, text=mp,
+        #    fg=colors.black, bg=colors.white)
 
 def main(): 
     ''' Starts the game '''
     with Cursebox() as cb:  
         width, height = cb.width, cb.height
         greeting = "Schiffeversenken"
+        test = "lkafnoknsskgnpesgn"
+        #cb.set_cursor(10,10)
         # Center text on the screen
         cb.put(x=(width - len(greeting)) / 2,
             y=height / 2, text=greeting,
             fg=colors.black, bg=colors.white)
         # Wait for any keypress
-        event = cb.poll_event()
-        if event == EVENT_ENTER:
-            cb.clear()
-            beginn_screen(width, height)
+        cb.poll_event()
+        cb.clear()
+        beginn_screen(width, height)
+        #cb.put_arrow(10,10,0,colors.black,colors.white)
+        #cb.clear()
+        #äif event == EVENT_UP:
+            #cb.clear
+            #beginn_screen(width, height)
+            #cb.put(x=(width - len(test)) / 2,
+            #    y=height / 2, text=test,
+            #    fg=colors.black, bg=colors.white)
+            #cb.refresh()
     
 
 
