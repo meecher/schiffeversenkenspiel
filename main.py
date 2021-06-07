df3941c87f5cd82e0ca695b7cb92f66ea14c1343
meecher <marcullmann@web.de>
2021-06-07 03:46:49 +0200

    translated to different plugin

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 ''' The game Schiffeversenken. '''
 
 import random, time
-import keyboard
-from cursebox import *
-from cursebox.constants import EVENT_BACKSPACE, EVENT_DOWN, EVENT_ENTER, EVENT_ESC, EVENT_LEFT, EVENT_RIGHT, EVENT_UP
-
+from re import escape
+#import keyboard
+import curses
 
 __author__ = "1359831, Ruschmaritsch, 1357985, Ullmann"
 __credits__ = ""
@@ -13,66 +12,94 @@ __email__ = "david.ruschmaritsch@stud.fra-uas.de, marc.ullmann@stud.fra-uas.de"
 def options():
     print("hi")
 
-def init_game(playerdefine):
-    print("hi")
+def init_game(stdscr, mode):
+    stdscr.addstr(curses.LINES // 2,
+    curses.COLS // 2 - len(mode) // 2,
+    mode, curses.A_BLINK)
 
-def blink(state):
-    activestate = 0
-    while state > 0:
-        if activestate == 0:
-            activestate = 1
-        else: 
-            activestate = 0
-    return activestate
+def beginn_screen(stdscr): 
+    stdscr.keypad(1)
+    comp = "Computer"
+    mp = "2 Spieler"
+    currselction = "mp"
+    stdscr.addstr(curses.LINES // 2,
+    curses.COLS // 2 - len(comp) // 2 + 10,
+    comp, curses.A_BLINK)
+    stdscr.addstr(curses.LINES // 2,
+    curses.COLS // 2 - len(mp) // 2 - 10,
+    mp, curses.A_REVERSE) 
+    stdscr.refresh()
 
+    while True:
+    #Highlights current selected item
+        curinput = stdscr.get_wch()
+        if curinput == 'a' or curinput == curses.KEY_LEFT:    
+        #Left side highlight (mulitplayer)
+            stdscr.erase()
+            stdscr.refresh()
+            stdscr.addstr(curses.LINES // 2,
+            curses.COLS // 2 - len(comp) // 2 + 10,
+            comp, curses.A_BLINK)
+            stdscr.addstr(curses.LINES // 2,
+            curses.COLS // 2 - len(mp) // 2 - 10,
+            mp, curses.A_REVERSE) 
+            stdscr.refresh()
+            currselction = "mp"
+        elif curinput == 'd' or curinput == curses.KEY_RIGHT:
+        #Right side highlight (computer)
+            stdscr.erase()
+            stdscr.refresh()
+            stdscr.addstr(curses.LINES // 2,
+            curses.COLS // 2 - len(comp) // 2 + 10,
+            comp, curses.A_REVERSE)
+            stdscr.addstr(curses.LINES // 2,
+            curses.COLS // 2 - len(mp) // 2 - 10,
+            mp, curses.A_BLINK)
+            stdscr.refresh()
+            currselction = "comp"
+        elif curinput == 'o' or curinput == "\n":
+            stdscr.erase()
+            stdscr.refresh()
+            init_game(stdscr, currselction) 
 
-def beginn_screen(width, height):
-    with Cursebox() as cb:  
-        comp = "Computer"
-        mp = "2 Spieler"
-        currselction = ""
-        cb.clear()
-        cb.put(x=(width - len(comp)) / 2 + 10,
-        y=height / 2, text=comp, fg=colors.white, bg=colors.black)
-        cb.put(x=(width - len(mp)) / 2 - 10,
-        y=height / 2, text=mp, fg=colors.black, bg=colors.white)
-        cb.refresh()
-
-        while True:
-        #Highlights current selected item
-            if keyboard.is_pressed('left arrow') or keyboard.is_pressed('a'):
-            #Left side highlight
-                cb.clear()
-                currselction = "left"
-                cb.put(x=(width - len(comp)) / 2 + 10,
-                y=height / 2, text=comp, fg=colors.white, bg=colors.black)
-                cb.put(x=(width - len(mp)) / 2 - 10,
-                y=height / 2, text=mp, fg=colors.black, bg=colors.white)
-                cb.refresh()
-            elif keyboard.is_pressed('right arrow') or keyboard.is_pressed('d'):
-            #Right side highlight
-                cb.clear()
-                currselction = "right"
-                cb.put(x=(width - len(comp)) / 2 + 10,
-                y=height / 2, text=comp, fg=colors.black, bg=colors.white)
-                cb.put(x=(width - len(mp)) / 2 - 10,
-                y=height / 2, text=mp, fg=colors.white, bg=colors.black)
-                cb.refresh()
-                
-          
-def main(): 
+def c_main(stdscr): 
     ''' Starts the game '''
-    with Cursebox() as cb:  
-        width, height = cb.width, cb.height
-        greeting = "Schiffeversenken"
-        # Center text on the screen
-        cb.put(x=(width - len(greeting)) / 2,
-            y=height / 2, text=greeting,
-            fg=colors.black, bg=colors.white)
-        # Wait for any keypress
-        cb.poll_event()
-        beginn_screen(width, height)
+    beginning = "Schiffeversenken"
+    curses.mousemask(-1)
+    curses.mouseinterval(0)
+    curses.curs_set(0)
+    stdscr.keypad(True)
 
+    pressed = False
+    x, y = 0, 0
+    while True: 
+
+        stdscr.addstr(curses.LINES // 2,
+        curses.COLS // 2 - len(beginning) // 2,
+        beginning, curses.A_REVERSE)
+        if pressed:
+            stdscr.addstr(y,x, "x")
+
+        c = stdscr.get_wch()
+        if c == 'q':
+            return 0
+        elif c == '\n':
+            stdscr.erase()
+            stdscr.refresh()
+            beginn_screen(stdscr)
+        elif c == curses.KEY_MOUSE:
+            _, x, y, _, bstate = curses.getmouse()
+            if bstate & curses.BUTTON1_PRESSED:
+                pressed = True
+            elif bstate & curses.BUTTON1_RELEASED:
+                pressed = False
+            #stdscr.addstr(1, 0, 'mouse')
+        else:  
+            raise AssertionError(c)
+ 
+
+def main():
+    return curses.wrapper(c_main)    
 
 if __name__ == "__main__":
     main()
---
+++ test2.py
@@ -0,0 +1,8 @@
+import time
+
+back = open(r"C:\Users\marcu\Documents\Uni\BSRN\Schiffeversenken\schiffeversenkenspiel\bg.txt", "r")
+#back = open("bg.txt")
+content = back.read()
+print(content)
+
+time.sleep(10)
