54f959d3db776f0f5808e5c676acec00a7bb3a61
meecher <marcullmann@web.de>
2021-06-13 19:28:19 +0200

    fo
    Co-authored-by: Lotte13 <Lotte13@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 ''' The game Schiffeversenken. '''
 
 import random, time
+import numpy as np
 import curses
 
 __author__ = "1359831, Ruschmaritsch, 1357985, Ullmann"
@@ -13,23 +14,81 @@ class ship:
     def __init__(self, size, rotation, position):
         self.size = size
 
+def create_matchfield(ySize, xSize, screen):
+    #global matchfield1
+    #matchfield1 = np.zeros((ySize, xSize))
+    matchfield1 = np.zeros((ySize, xSize))
+    set_ships(0,0,1,matchfield1,screen)
+    #atchfield1_visual = str(matchfield1)
+    #screen.addstr(0,0,str(matchfield1_visual))
+    #print(matchfield1)
+    #global matchfield2
+    #screen.addstr(0,0,np.zeros((ySize, xSize))
+    #matchfield2 = np.zeros((ySize, xSize))
+
+def update_matchfield(yPos, xPos, matchfield, player, screen):
+    matchfield1_visual = str(matchfield)
+    screen.clear()
+    screen.addstr(0,0,str(matchfield))
+    screen.refresh()
+    return
+    #set_ships(yPos, xPos, matchfield, player, screen)
+
+def set_ships(yPos, xPos, matchfield, player, screen):
+   # global matchfield1
+
+    key = ''
+    #while key != ord('q'):
+    while True:
+        key = screen.getch()
+        print(key)
+        if key == curses.KEY_RIGHT:
+            print("right")
+            matchfield[yPos, xPos] = 0
+            xPos += 1
+            matchfield[yPos, xPos] = 1
+            update_matchfield(matchfield,screen)
+        #screen.addstr(0, 0, str(matchfield))
+        #screen.refresh()
+        if key == curses.KEY_DOWN:
+            print("down")
+            matchfield[yPos, xPos] = 0
+            yPos += 1
+            matchfield[yPos, xPos] = 1
+            update_matchfield(matchfield,screen)
+        #screen.refresh()
+        if key == curses.KEY_LEFT:
+            print("left")
+            matchfield[yPos, xPos] = 0
+            xPos -= 1
+            matchfield[yPos, xPos] = 1
+            update_matchfield(matchfield,screen)
+        #screen.refresh()
+        if key == curses.KEY_UP:
+            print("up")
+            matchfield[yPos, xPos] = 0
+            yPos -= 1
+            matchfield[yPos, xPos] = 1
+            update_matchfield(matchfield,screen)
+        #screen.refresh()
 
 def options():
     ''' Currently not in use '''
     print("hi")
 
-def init_game(stdscr, mode):
+def init_game(screen, mode):
     ''' Starts selected game mode '''
-    stdscr.addstr(curses.LINES // 2,
-    curses.COLS // 2 - len(mode) // 2,
-    mode, curses.A_BLINK)
-    stdscr.refresh()
-    curinput = stdscr.get_wch() 
+    #screen.addstr(curses.LINES // 2,
+    #curses.COLS // 2 - len(mode) // 2,
+    #mode, curses.A_BLINK)
+    screen.refresh()
+    create_matchfield(10,10,screen)
 
 
-def beginn_screen(stdscr): 
+
+def beginn_screen(screen): 
     ''' Let's the player select the game mode '''
-    stdscr.keypad(1)
+    screen.keypad(1)
     curses.mousemask(-1)
 
     comp = "Computer"
@@ -40,17 +99,17 @@ def beginn_screen(stdscr):
     mouse_press = False
     x, y = 0, 0
 
-    stdscr.addstr(curses.LINES // 2,
+    screen.addstr(curses.LINES // 2,
     curses.COLS // 2 - len(mp) // 2 - 10,
     mp, curses.A_REVERSE) 
-    stdscr.addstr(curses.LINES // 2,
+    screen.addstr(curses.LINES // 2,
     curses.COLS // 2 - len(comp) // 2 + 10,
     comp, curses.A_BLINK)
-    stdscr.refresh()
+    screen.refresh()
 
     while True:
     # Highlights current selected item; waits for input
-        curinput = stdscr.get_wch() 
+        curinput = screen.get_wch() 
                  
         if curinput == 'a' or curinput == curses.KEY_LEFT or pressed:    
         # Checks for key or mouse press on the left side (multiplayer)
@@ -66,21 +125,21 @@ def beginn_screen(stdscr):
                 check_press = True  
             if check_press == True:
             # Draws updated text
-                stdscr.erase()
-                stdscr.refresh()
-                stdscr.addstr(curses.LINES // 2,
+                screen.erase()
+                screen.refresh()
+                screen.addstr(curses.LINES // 2,
                 curses.COLS // 2 - len(comp) // 2 + 10,
                 comp, curses.A_BLINK)
-                stdscr.addstr(curses.LINES // 2,
+                screen.addstr(curses.LINES // 2,
                 curses.COLS // 2 - len(mp) // 2 - 10,
                 mp, curses.A_REVERSE) 
-                stdscr.refresh()
+                screen.refresh()
                 currselction = "mp"
                 if mouse_press == True:
                 # Confirms selection if mouse is pressed       
-                    stdscr.erase()
-                    stdscr.refresh()
-                    init_game(stdscr, currselction)
+                    screen.erase()
+                    screen.refresh()
+                    init_game(screen, currselction)
 
         if curinput == 'd' or curinput == curses.KEY_RIGHT or pressed:
         # Checks for key or mouse press on the right side (singleplayer)
@@ -96,27 +155,27 @@ def beginn_screen(stdscr):
                 check_press = True
             if check_press == True:
             # Draws updated text   
-                stdscr.erase()
-                stdscr.refresh()
-                stdscr.addstr(curses.LINES // 2,
+                screen.erase()
+                screen.refresh()
+                screen.addstr(curses.LINES // 2,
                 curses.COLS // 2 - len(comp) // 2 + 10,
                 comp, curses.A_REVERSE)
-                stdscr.addstr(curses.LINES // 2,
+                screen.addstr(curses.LINES // 2,
                 curses.COLS // 2 - len(mp) // 2 - 10,
                 mp, curses.A_BLINK)
-                stdscr.refresh()
+                screen.refresh()
                 currselction = "comp"
                 if mouse_press == True: 
                 # Confirms selection if mouse is pressed      
-                    stdscr.erase()
-                    stdscr.refresh()
-                    init_game(stdscr, currselction) 
+                    screen.erase()
+                    screen.refresh()
+                    init_game(screen, currselction) 
             
         elif curinput == '\n':    
         # Enter for selection; starts new function with selected gamemode
-            stdscr.erase()
-            stdscr.refresh()
-            init_game(stdscr, currselction) 
+            screen.erase()
+            screen.refresh()
+            init_game(screen, currselction) 
 
         if curinput == curses.KEY_MOUSE:
         # Handles mouse input
@@ -133,20 +192,20 @@ def mouse_state():
     _, x, y, _, bstate = curses.getmouse()
     return x, y, bstate
 
-def c_main(stdscr): 
+def c_main(screen): 
     ''' Starts the game '''
     beginning = "Schiffeversenken"
     curses.mousemask(-1)
     curses.mouseinterval(0)
     curses.curs_set(0)
-    stdscr.keypad(True)
+    screen.keypad(True)
 
     pressed = False
     x, y = 0, 0
 
     while True: 
     # Waits for input
-        stdscr.addstr(curses.LINES // 2,
+        screen.addstr(curses.LINES // 2,
         curses.COLS // 2 - len(beginning) // 2,
         beginning, curses.A_REVERSE)
 
@@ -154,19 +213,19 @@ def c_main(stdscr):
         # Checks if mouse is pressed
             if y == curses.LINES // 2 and x in range((curses.COLS // 2 - len(beginning) // 2 + 10)-10,(curses.COLS // 2 - len(beginning) // 2 + 10)+7): 
             # Checks if desired coordinates are pressed for the main menu button
-                stdscr.erase()
-                stdscr.refresh()
-                beginn_screen(stdscr)
+                screen.erase()
+                screen.refresh()
+                beginn_screen(screen)
 
-        curinput = stdscr.get_wch()
+        curinput = screen.get_wch()
         if curinput == 'q':
         # Quits programm when the key q is pressed
             return 0
         elif curinput == '\n':
         # Starts game if enter is pressed
-            stdscr.erase()
-            stdscr.refresh()
-            beginn_screen(stdscr)
+            screen.erase()
+            screen.refresh()
+            beginn_screen(screen)
         elif curinput == curses.KEY_MOUSE:
         # Handles mouse input
             x, y, bstate = mouse_state()
